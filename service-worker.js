aa7ff2c99b52c5fe30a3877e9d4217ee7f36d586
chidea <sbw228@gmail.com>
2020-05-28 13:02:31 +0900

    .

--- service-worker.js
+++ service-worker.js
@@ -6,6 +6,20 @@ var files = [
   'creole-live-editor.wasm'
 ];
 
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [cacheName];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
 
 /* Start the service worker and cache all of the app's content */
 self.addEventListener('install', e => {
@@ -21,6 +35,8 @@ self.addEventListener('fetch', e => {
   e.respondWith(
     caches.match(e.request).then(response => {
       return response || fetch(e.request);
+    }).catch(e => {
+      // offline without cache
     })
   );
 });
