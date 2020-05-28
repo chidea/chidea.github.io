5c832a733b67f382d5c9cb5481eb2a3298aa17c2
chidea <sbw228@gmail.com>
2020-05-28 12:49:43 +0900

    .

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,25 @@
-var cacheName = 'chideas-yew-pwaa';
-var filesToCache = [
-  './index.html',
-  './creole-live-editor.js',
-  './1.creole-live-editor.js',
-  './creole-live-editor.wasm'
+var cacheName = 'chideas-yew-pwaa-v0.0.1';
+var files = [
+  '/',
+  '1.creole-live-editor.js',
+  'creole-live-editor.js',
+  'creole-live-editor.wasm'
 ];
 
 
 /* Start the service worker and cache all of the app's content */
-self.addEventListener('install', function(e) {
+self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      return cache.addAll(filesToCache);
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(files);
     })
   );
 });
 
 /* Serve cached content when offline */
-self.addEventListener('fetch', function(e) {
+self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(function(response) {
+    caches.match(e.request).then(response => {
       return response || fetch(e.request);
     })
   );
