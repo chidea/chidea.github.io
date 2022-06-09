43e3c6f5264e0f106ca2502679c43fefef6dfed0
chidea <sbw228@gmail.com>
2022-06-09 17:53:24 +0900

    media layout for print

--- wasm-creole-live-editor-bfb45057a86e2942.js
+++ wasm-creole-live-editor-bfb45057a86e2942.js
@@ -269,7 +269,7 @@ async function load(module, imports) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('wasm-creole-live-editor-a2473e38db71061f_bg.wasm', import.meta.url);
+        input = new URL('wasm-creole-live-editor-bfb45057a86e2942_bg.wasm', import.meta.url);
     }
     const imports = {};
     imports.wbg = {};
